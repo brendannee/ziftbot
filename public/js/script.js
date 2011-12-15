f417498a0bddb6b886e1762464cc2b22b269cbb4
Brendan Nee <brendan.nee@gmail.com>
2011-12-14 23:33:44 -0800

    Added support for end of questions and start over

--- public/js/script.js
+++ public/js/script.js
@@ -23,11 +23,17 @@ $(document).ready(function(){
       $.getJSON('/api/demographics/' + next_question, displayQuestions);
     } else {
       //start product questions
-      $.getJSON('/api/questions', {
-        recipient: recipient,
-        gender: gender,
-				ignore: ignore.join(',')
-        }, displayQuestions);
+			$.ajax({
+			  	url: '/api/questions'
+			  ,	dataType: 'json'
+				, data: {
+        	  recipient: recipient
+       	 	, gender: gender
+					, ignore: ignore.join(',')
+        	}
+			  , success: displayQuestions
+				, error: questionError
+			});
     }
     
     return false;
@@ -62,11 +68,7 @@ function displayQuestions(data, textStatus, jqXHR){
     };
     
     //Create div, add stuff then append as a question
-    var questionDiv = $('<div/>',{
-      class:"question"
-    });
-    
-    $(questionDiv)
+    $('<div/>',{ class:"question" })
       .append($('<div/>',{
         class:"questionText",
         html:data.q
@@ -78,16 +80,41 @@ function displayQuestions(data, textStatus, jqXHR){
       .appendTo('#questions');
       
       //scroll questions
-      var indentation = parseInt($('#questions').css('marginLeft'), 10) - 600;
-      $("#questions").animate({
-         marginLeft: indentation + 'px',
-       }, {
-         duration: 'slow',
-         easing: 'easeOutQuint'
-       });
+    	scrollQuestions();
       
       
   } else {
     console.log(textStatus + ' Error retrieving question');
   }
 };
+
+function questionError(jqXHR, textStatus) {
+	if(jqXHR.status == 402){
+		//out of questions, so clear all variables 
+		recipient = '';
+		gender = '';
+		ignore = [];
+		
+    $('#questions').append('<div class="question">' +
+      '<div class="questionText">Thats all the gift ideas we\'ve got.  Would you like to try again?</div>' +
+			'<div class="answers">' +
+			'<a class="btn large primary" data-next="1" >Sure, why not?</a>' +
+			'</div>'
+		);
+			  
+		scrollQuestions();
+		
+	} else {
+		console.log(jqXHR);
+	}
+}
+
+function scrollQuestions(){
+  var indentation = parseInt($('#questions').css('marginLeft'), 10) - parseInt($('#questionsFrame').css('width'), 10);
+  $("#questions").animate({
+     marginLeft: indentation + 'px',
+  }, {
+     duration: 'slow',
+     easing: 'easeOutQuint'
+  });
+}
