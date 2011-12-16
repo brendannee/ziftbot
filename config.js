4768c9ca2089b3e0d9b0e29831ec1c947174d466
Conner Petzold <cpetzold@gmail.com>
2011-12-16 00:06:05 -0800

    added stylus and nib

--- config.js
+++ config.js
@@ -1,22 +1,12 @@
 var express = require('express')
   , expressNamespace = require('express-namespace')
+  , stylus = require('stylus')
+  , nib = require('nib')
   , Zappos = require('zappos');
   
 module.exports = function(app) {
   
-  var key = process.env.ZAPPOS_KEY || require('./key');
-	app.set('zappos', new Zappos(key));
   
-  app.configure(function() {
-    this.use(express.cookieParser())
-        .use(express.bodyParser())
-        .set('views', __dirname + '/views')
-        .set('view engine', 'jade')
-        .set('public', __dirname + '/public')
-        .enable('error templates')
-        .use(express.static(__dirname + '/public'))
-  });
-
   app.configure('development', function(){
     this
       .use(express.logger('\x1b[90m:remote-addr -\x1b[0m \x1b[33m:method\x1b[0m' +
@@ -33,5 +23,32 @@ module.exports = function(app) {
       .enable('prod')
       .set('domain', 'ziftbot.com');
   });  
+
+  app.configure(function() {
+    var key = process.env.ZAPPOS_KEY || require('./key');
+    app.set('zappos', new Zappos(key));
+
+    this.use(express.cookieParser())
+        .use(express.bodyParser())
+        .set('views', __dirname + '/views')
+        .set('view engine', 'jade')
+        .set('public', __dirname + '/public')
+        .enable('error templates')
+        .use(express.static(this.set('public')))
+        .use('/css', stylus.middleware({
+            force: true
+          , compress: true
+          , debug: true
+          , src: __dirname + '/styles'
+          , dest: this.set('public') + '/css'
+          , compile: function(str, path) {
+              return stylus(str)
+                .set('filename', path)
+                .set('paths', ['/style'])
+                .use(nib());
+            }
+        }))
+  });
+
   
 }
